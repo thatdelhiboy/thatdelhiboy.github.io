8e075c85555ba4765c1c5b99fa488948451e935e
Abhishek Singh <47791443+thatdelhiboy@users.noreply.github.com>
2020-02-21 16:36:48 +0530

    contact update

--- js/main.js
+++ js/main.js
@@ -2,8 +2,9 @@
 					Js Main
 --------------------------------------------------
 
-    Template Name: Baha - Personal Portfolio Template
-    Author: Malyarchuk
+    Template Name: Personal Portfolio Template
+    Author: Abhishek Singh
+    Email-anchor2abhishek@gmail.com
     Copyright: 2019
 
 --------------------------------------------------
@@ -232,37 +233,37 @@ $(document).ready(function() {
         });
     }
 	
-	/* Google Map Setup */
-    if($('#map').length) {
-        initMap();
-     };
+	// /* Google Map Setup */
+    // if($('#map').length) {
+    //     initMap();
+    //  };
 
 });
 
 /* -----------------------------------
   		14. Google Map
 ----------------------------------- */
-function initMap() {
-    var latitude = $("#map").data('latitude'),
-        longitude = $("#map").data('longitude'),
-        zoom = $("#map").data('zoom'),
-        cordinates = new google.maps.LatLng(latitude, longitude);
+// function initMap() {
+//     var latitude = $("#map").data('latitude'),
+//         longitude = $("#map").data('longitude'),
+//         zoom = $("#map").data('zoom'),
+//         cordinates = new google.maps.LatLng(latitude, longitude);
 
-    var styles = [{"stylers":[{"saturation":-100},{"gamma":0.8},{"lightness":4},{"visibility":"on"}]},{"featureType":"landscape.natural","stylers":[{"visibility":"on"},{"color":"#5dff00"},{"gamma":4.97},{"lightness":-5},{"saturation":100}]}];
+//     var styles = [{"stylers":[{"saturation":-100},{"gamma":0.8},{"lightness":4},{"visibility":"on"}]},{"featureType":"landscape.natural","stylers":[{"visibility":"on"},{"color":"#5dff00"},{"gamma":4.97},{"lightness":-5},{"saturation":100}]}];
 	
-        var mapOptions = {
-        zoom: zoom,
-        center: cordinates,
-        mapTypeControl: false,
-        disableDefaultUI: true,
-        zoomControl: true,
-        scrollwheel: false,
-        styles: styles
-    };
-    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-    var marker = new google.maps.Marker({
-        position: cordinates,
-        map: map,
-        title: "We are here!"
-    });
-}
+//         var mapOptions = {
+//         zoom: zoom,
+//         center: cordinates,
+//         mapTypeControl: false,
+//         disableDefaultUI: true,
+//         zoomControl: true,
+//         scrollwheel: false,
+//         styles: styles
+//     };
+//     var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+//     var marker = new google.maps.Marker({
+//         position: cordinates,
+//         map: map,
+//         title: "We are here!"
+//     });
+// }
